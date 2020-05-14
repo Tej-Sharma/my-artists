d8c5bcbcdcf29d1911d579228365f5a89111c073
Tejas Sharma <tejassharma08@gmail.com>
2020-05-14 19:43:19 +0200

    Added credentials

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ import * as firebase from "firebase/app";
 // IMPORTANT Note: the IDs below have been asteriked because this is private data
 // and should not be shared on a public GitHub repository
 const firebaseConfig = {
-  apiKey: "**************************",
-  authDomain: "******************",
-  databaseURL: "****************",
-  projectId: "************",
-  storageBucket: "******************",
-  messagingSenderId: "**************",
-  appId: "***********************",
-  measurementId: "************"
+  apiKey: "AIzaSyBraqgKxrrqLBkk6Jf4Y8b_H-47AoJjCKY",
+  authDomain: "softie-pals.firebaseapp.com",
+  databaseURL: "https://softie-pals.firebaseio.com",
+  projectId: "softie-pals",
+  storageBucket: "softie-pals.appspot.com",
+  messagingSenderId: "2015829189",
+  appId: "1:2015829189:web:178dfbed2c1c0c7453e4ac",
+  measurementId: "G-WL8J92DEXV"
 };
 
 // Initialize the app using the config
